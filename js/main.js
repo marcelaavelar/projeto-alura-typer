41fd0c374f893383bfd79dac81b6d2a090699cf7
Marcela Avelar <marcela.lilian@gmail.com>
2018-08-14 18:52:03 -0300

    Projeto Inicial

---
+++ js/main.js
@@ -0,0 +1,79 @@
+var tempoInicial = $("#tempo-digitacao").text();
+var campo = $(".campo-digitacao");
+
+$(document).ready(function(){
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    inicializaMarcadores();
+    $("#botao-reiniciar").click(reiniciaJogo);
+});
+
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(/\S+/).length -1;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
+
+function inicializaContadores() {
+    campo.on("input", function() {
+        var conteudo = campo.val();
+    
+        var qntPalavras = conteudo.split(/\S+/).length -1;
+            $("#contador-palavras").text(qntPalavras);
+    
+        var qntCaracteres = conteudo.length;
+            $("#contador-caracteres").text(qntCaracteres);
+    });
+}
+
+function inicializaCronometro() {
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function() {
+        var cronometroID = setInterval(function() {
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1){
+                clearInterval(cronometroID);
+                finalizaJogo();
+            }
+        },1000);
+    });
+}
+
+function finalizaJogo(){
+    campo.attr("disabled", true);
+    campo.toggleClass("campo-desativado");
+    inserePlacar();
+}
+
+function inicializaMarcadores(){
+    var frase = $(".frase").text();
+    campo.on("input", function(){
+    
+        var digitado = campo.val();
+        var comparavel = frase.substr(0, digitado.length);
+        console.log(comparavel);
+        if (digitado == comparavel){
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        }else {
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
+    });
+}
+
+function reiniciaJogo(){
+    campo.attr("disabled", false);
+    campo.val("");
+
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+    campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde");
+}
