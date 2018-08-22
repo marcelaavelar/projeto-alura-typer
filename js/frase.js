690d05c960e485a7b27cbc53b715b8ffc9cac1ff
Marcela Avelar <marcela.lilian@gmail.com>
2018-08-22 10:47:14 -0300

    atualizado

--- js/frase.js
+++ js/frase.js
@@ -3,7 +3,7 @@ $("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria(){
     $("#spinner").toggle();
-    $.get("http://localhost:3001/frases",trocaFraseAleatoria)
+    $.get("http://localhost:3000/frases",trocaFraseAleatoria)
     .fail(function(){
         $("#erro").toggle();
         setTimeout(function(){
@@ -30,7 +30,7 @@ function buscaFrase(){
     
     var dados = {id: fraseId};
 
-    $.get("http://localhost:3001/frases",dados,trocaFrase)
+    $.get("http://localhost:3000/frases",dados,trocaFrase)
     .fail(function(){
         $("#erro").toggle();
         setTimeout(function(){
